e37edefb36dd982979bd97c1598b09e492e3e08d
Tomas Morstein <tmr@idea.cz>
2014-05-20 21:43:03 +0200

    add some missing sudos

--- build-kali.sh
+++ build-kali.sh
@@ -4,6 +4,6 @@
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=HEAD" > kali-debootstrap &&\
 sudo debootstrap kali ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
 sudo tar -C kali-root -c . | sudo docker import - linux/kali &&\
-rm -rf ./kali-root &&\
-docker run -t -i linux/kali cat /etc/debian_version &&\
+sudo rm -rf ./kali-root &&\
+sudo docker run -rm -t -i linux/kali cat /etc/debian_version &&\
 echo "Build OK" || echo "Build failed!"
